9c84a9e1090305c9431ee4c752fad16aedcaf0eb
97guido <39537574+97guido@users.noreply.github.com>
2025-09-02 18:39:56 +0200

    Add files via upload

---
+++ elencoCiboBevande.js
@@ -0,0 +1,208 @@
+var items = [
+    {
+        "name": "Birra bottiglia [€3]",
+        "prezzo": 3,
+        "quantita": 0,
+        "id_num": "num_birra_bott",
+        "colore": "#fff899",
+        "categorie": "bar"
+    },
+    {
+        "name": "Birra alla spina PICCOLA [€3,50]",
+        "prezzo": 3.5,
+        "quantita": 0,
+        "id_num": "num_birra_pic",
+        "colore": "#fff899",
+        "categorie": "bar"
+    },
+    {
+        "name": "Birra alla spina MEDIA [€5]",
+        "prezzo": 5,
+        "quantita": 0,
+        "id_num": "num_birra_med",
+        "colore": "#fff899",
+        "categorie": "bar"
+    },
+    {
+        "name": "Bibita lattina [€2,50]",
+        "prezzo": 2.5,
+        "quantita": 0,
+        "id_num": "num_lattina",
+        "colore": "#ff9999",
+        "categorie": "bar"
+    },
+    {
+        "name": "Bottiglia vino 1/4L [€2]",
+        "prezzo": 2,
+        "quantita": 0,
+        "id_num": "num_bott_vino14",
+        "colore": "#e799ff",
+        "categorie": "bar"
+    },
+    {
+        "name": "Bottiglia vino 0,75L [€5]",
+        "prezzo": 5,
+        "quantita": 0,
+        "id_num": "num_bott_vino075",
+        "colore": "#e799ff",
+        "categorie": "bar"
+    },
+    {
+        "name": "Bottiglia acqua GRANDE [€2]",
+        "prezzo": 2,
+        "quantita": 0,
+        "id_num": "num_acqua_grand",
+        "colore": "#99f0ff",
+        "categorie": "bar"
+    },
+    {
+        "name": "Bottiglia acqua PICCOLA [€1]",
+        "prezzo": 1,
+        "quantita": 0,
+        "id_num": "num_acqua_pic",
+        "colore": "#99f0ff",
+        "categorie": "bar"
+    },
+    {
+        "name": "Caffè [€1,20]",
+        "prezzo": 1.2,
+        "quantita": 0,
+        "id_num": "num_caffe",
+        "colore": "#a78c7c",
+        "categorie": "bar"
+    },
+    {
+        "name": "Caffè corretto [€1,50]",
+        "prezzo": 1.5,
+        "quantita": 0,
+        "id_num": "num_caffe_corr",
+        "colore": "#a78c7c",
+        "categorie": "bar"
+    },
+    {
+        "name": "Liquore [€3]",
+        "prezzo": 3,
+        "quantita": 0,
+        "id_num": "num_liquore",
+        "colore": "#988fa3",
+        "categorie": "bar"
+    },
+
+
+
+
+
+    {
+        "name": "Solo piada [€1]",
+        "prezzo": 1,
+        "quantita": 0,
+        "id_num": "num_piada",
+        "colore": "#ffcf99",
+        "categorie": "cibo"
+    },
+    {
+        "name": "Piada con prosciutto [€7]",
+        "prezzo": 7,
+        "quantita": 0,
+        "id_num": "num_piada_pros",
+        "colore": "linear-gradient(to right, #ffcf99 , #ff9999)",
+        "categorie": "cibo"
+    },
+    {
+        "name": "Piada crudo, casatella e rucola [€7,50]",
+        "prezzo": 7.5,
+        "quantita": 0,
+        "id_num": "num_piada_cru_ca_ru",
+        // "colore": "#ffcf99"
+        "colore": "linear-gradient(to right, #ff9999, white, #dbff99)",
+        "categorie": "cibo"
+    },
+    {
+        "name": "Piada con salame [€6]",
+        "prezzo": 6,
+        "quantita": 0,
+        "id_num": "num_piada_salame",
+        "colore": "linear-gradient(to right, #ffcf99 , #ff9999)",
+        "categorie": "cibo"
+    },
+    {
+        "name": "Piada con salsiccia [€7]",
+        "prezzo": 7,
+        "quantita": 0,
+        "id_num": "num_piada_salsic",
+        "colore": "linear-gradient(to right, #ffcf99 , #a78c7c)",
+        "categorie": "cibo"
+    },
+    {
+        "name": "Piada casatella e rucola [€7]",
+        "prezzo": 7,
+        "quantita": 0,
+        "id_num": "num_piada_casa_ruc",
+        "colore": "linear-gradient(to right, white, #dbff99)",
+        "categorie": "cibo"
+    },
+    {
+        "name": "Piada con Gratin (4 pz.) [€7]",
+        "prezzo": 7,
+        "quantita": 0,
+        "id_num": "num_piada_grat",
+        "colore": "linear-gradient(to right, #ff9999, #dbff99)",
+        "categorie": "cibo"
+    },
+    {
+        "name": "Strozzapreti [€7,50]",
+        "prezzo": 7.5,
+        "quantita": 0,
+        "id_num": "num_strozzap",
+        "colore": "#ffad99",
+        "categorie": "cibo"
+    },
+    {
+        "name": "Trippa [€7,50]",
+        "prezzo": 7.5,
+        "quantita": 0,
+        "id_num": "num_trippa",
+        "colore": "#ffad99",
+        "categorie": "cibo"
+    },
+    {
+        "name": "Cotiche con fagioli [€7]",
+        "prezzo": 7,
+        "quantita": 0,
+        "id_num": "num_cotiche_fagio",
+        "colore": "#ffad99",
+        "categorie": "cibo"
+    },
+    {
+        "name": "Gratin (4 pz.) [€6]",
+        "prezzo": 6,
+        "quantita": 0,
+        "id_num": "num_gratin",
+        "colore": "#dbff99",
+        "categorie": "cibo"
+    },
+    {
+        "name": "Insalata [€2,50]",
+        "prezzo": 2.5,
+        "quantita": 0,
+        "id_num": "num_insalata",
+        "colore": "#dbff99",
+        "categorie": "cibo"
+    },    
+    {
+        "name": "Patatine [€3]",
+        "prezzo": 3,
+        "quantita": 0,
+        "id_num": "num_pata",
+        "colore": "#fff899",
+        "categorie": "cibo,bar"
+    },
+    {
+        "name": "Hamburger [€7]",
+        "prezzo": 7,
+        "quantita": 0,
+        "id_num": "num_hamb",
+        "colore": "#ff9999",
+        "categorie": "cibo,bar"
+    },
+]
---
+++ main.js
@@ -0,0 +1,198 @@
+var tot = 0;
+var loading = false;
+
+function init() {
+    setButtonNames();
+    setAllItems();
+}
+
+function setAllItems() {
+
+    items.forEach(item => {
+
+        var newDiv = createListItem(item);
+    
+        if (item.categorie.includes("cibo")) document.querySelectorAll('#div_mangiare')[0].appendChild(newDiv);
+        if (item.categorie.includes("bar")) document.querySelectorAll('#div_bere')[0].appendChild(newDiv);
+        if (item.categorie.includes("cibo") && item.categorie.includes("bar")) {
+            var newDivDup = createListItem(item);
+            document.querySelectorAll('#div_mangiare')[0].appendChild(newDivDup);
+        };
+    });   
+}
+
+function createListItem(item) {
+    var newDiv = document.createElement('div');
+    newDiv.classList.add("itemBox");
+
+    var newBtn = document.createElement('input');
+    newBtn.id = item.id_num.replaceAll('num_', 'btn_');
+    newBtn.type = "button";
+    newBtn.value = item.name;
+    newBtn.style.background = item.colore;
+    newBtn.classList.add("label_btn");
+    newBtn.onclick = function() { addToItem(item.id_num) };
+    
+    var newBtnDel = document.createElement('input');
+    newBtnDel.value = "-";
+    newBtnDel.type = "button";
+    newBtnDel.classList.add("del_btn");
+    newBtnDel.onclick = function() { removeToItem(item.id_num) };
+    
+    var newNum = document.createElement('input');
+    newNum.id = item.id_num;
+    newNum.type = "number";
+    newNum.value = 0;
+    newNum.oninput = function() { updateNumber(item.id_num) };
+
+    newDiv.appendChild(newBtn);
+    newDiv.appendChild(newBtnDel);
+    newDiv.appendChild(newNum);
+
+    return newDiv;
+}
+
+
+function setButtonNames() {
+    document.querySelectorAll('div > div > div > input[type=button]').forEach(el => {
+        if (el.id != '') {
+            var idToFind = el.id.replaceAll('btn_','num_');
+            items.forEach(item => {
+                if (idToFind == item.id_num) el.value = item.name;
+            });
+        }
+    });
+}
+
+function addToItem(idItem) {
+    
+    var numItem = -1;
+    
+    items.forEach((el, index) => {
+        if (el.id_num == idItem) numItem = index;
+    });
+
+    if (numItem == -1) return;
+
+    ++(items[numItem].quantita);
+    document.getElementById(items[numItem].id_num).value = items[numItem].quantita;
+    updateConto();
+}
+
+function removeToItem(idItem) {
+
+    var numItem = -1;
+    
+    items.forEach((el, index) => {
+        if (el.id_num == idItem) numItem = index;
+    });
+
+    if (numItem == -1) return;
+
+    if (items[numItem].quantita < 1) return;
+    --(items[numItem].quantita);
+    document.getElementById(items[numItem].id_num).value = items[numItem].quantita;
+    updateConto();
+}
+
+function updateNumber(idItem) {
+
+    var numItem = -1;
+    
+    items.forEach((el, index) => {
+        if (el.id_num == idItem) numItem = index;
+    });
+
+    if (numItem == -1) return;
+
+    var qty = Number(document.getElementById(items[numItem].id_num).value);
+    if (qty < 0) qty = 0;
+    items[numItem].quantita = qty;
+    updateConto();
+}
+
+function updateConto() {
+    tot = 0;
+    
+    items.forEach(el => {
+        tot += el.prezzo * el.quantita;
+
+        if (document.getElementById(el.id_num) != undefined) {
+            document.getElementById(el.id_num).value = el.quantita;
+
+            document.querySelectorAll(`#${el.id_num}`).forEach(node => {
+                node.value = el.quantita;
+            });
+
+            if (el.quantita > 0) {
+                document.querySelectorAll(`#${el.id_num.replaceAll('num_', 'btn_')}`).forEach(node => { node.style.background = '#71c06a'; });
+                document.querySelectorAll(`#${el.id_num}`).forEach(node => { node.style.background = '#71c06a'; });
+            }
+            else {
+                document.querySelectorAll(`#${el.id_num.replaceAll('num_', 'btn_')}`).forEach(node => { node.style.background = el.colore; });
+                document.querySelectorAll(`#${el.id_num}`).forEach(node => { node.style.background = 'white'; });
+            }
+        }
+
+
+
+
+    });
+    document.getElementById('totBox').innerHTML = `Totale € ${tot.toFixed(2)}`.replaceAll('.', ',');
+}
+
+function confermaOrdine() {
+
+    if (items.filter(x => x.quantita > 0).length < 1) return
+
+    fetch('https://script.google.com/macros/s/AKfycbxgGI0HYYoNvC1s7ffy8oDHt4kPAD4I0Dh_miuAJq2wEWG4W800ARNxim-pBLoEjk7kYQ/exec?content=json',
+    {
+        method: 'POST',
+        body: `{ 
+            "tot": ${tot},
+            "items": ${JSON.stringify(items.filter(x => x.quantita > 0))} 
+        }`
+    }
+  ) // Example API call
+   .then(response => response.text())
+   .then(data => {
+      console.log('Data from API:', data);
+      azzeraCampi()
+      loadingEnd()
+      // Process the response data
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+      loadingEnd()
+    });
+
+    loadingStart()
+
+    
+}
+
+function azzeraCampi() {
+
+    tot = 0;
+    items.forEach(el => {
+        el.quantita = 0;
+    });
+    document.getElementById('totBox').innerHTML = 'Totale:';
+    updateConto();
+}
+
+function loadingStart() {
+    if (!loading) {
+        loading = true
+    }
+    document.getElementById("main_div").style.display = 'none'
+    document.getElementById("loading_div").style.display = 'block'
+}
+
+function loadingEnd() {
+    if (loading) {
+        loading = false
+    }
+    document.getElementById("main_div").style.display = 'block'
+    document.getElementById("loading_div").style.display = 'none'
+}
